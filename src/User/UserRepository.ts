41b89b704938f6c5ceaef847c5e730a4d436c5a1
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-02-08 19:39:10 -0300

    create UserRepository

--- src/User/UserRepository.ts
+++ src/User/UserRepository.ts
@@ -1,21 +1,24 @@
-// import { prisma, User } from "@prisma/client";
-// import { IUserRepository } from "./structure";
+import { User } from '@prisma/client';
+import prisma from '../database/client';
+import { IUserRepository } from "./structure";
 
-// interface ICreateUser {
-//     id?: string
-//     name: string
-//     email: string,
-//     passwordHash: string
-// }
+interface ICreateUser {
+    id?: string
+    name: string
+    email: string,
+    passwordHash: string
+}
 
-// export class UserRepository implements IUserRepository{
+export class UserRepository implements IUserRepository {
 
-//     async register(data: ICreateUser): Promise<User | Error> {
-        
-//     }
+    async register(data: ICreateUser): Promise<User | Error> {
+        const user = await prisma.user.create({ data })
+        return user
+    }
 
-//     async getByUser(value: string, key?: string): Promise<User | boolean> {
-//         const result = await prisma.user.
-//     }
+    async getByUser(value: string, key?: string): Promise<object|any> {
+        const result = await prisma.user.findUnique({where:{email:value}})
+        return result
+    }
 
-// }
+}
