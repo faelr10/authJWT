a73b10290318ab7b28093a534b10e9c693bf4026
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-02-08 15:09:05 -0300

    create test for Controller

---
+++ src/User/UserController.spec.ts
@@ -0,0 +1,44 @@
+import request from 'supertest'
+import { app } from '../app'
+import bcrypt from 'bcrypt'
+
+
+describe('Tests for Controller',()=>{
+
+    it('should validate name is not null',async()=>{
+        const user = await request(app)
+        .post('/register')
+        .send({
+            name:'Ed',
+            email:'rafael@rafael.com',
+            password:'123456',
+            confirmPassword:'123456'
+        })
+        expect(user.body.message).toBe('Field name not null!')
+    })
+
+    it('should validate email',async()=>{
+        const user = await request(app)
+        .post('/register')
+        .send({
+            name:'Rafael',
+            email:'rafaelrafael.com',
+            password:'123456',
+            confirmPassword:'123456'
+        })
+        expect(user.body.message).toBe('Email inválido!')
+    })
+
+    it('should validate compare password',async()=>{
+        const user = await request(app)
+        .post('/register')
+        .send({
+            name:'Rafael',
+            email:'rafael@rafael.com',
+            password:'123456',
+            confirmPassword:'123455'
+        })
+        expect(user.body.message).toBe('As senhas não conferem!')
+    })
+
+})
