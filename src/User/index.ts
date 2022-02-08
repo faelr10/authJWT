a1d6bdb72cee368f503172ffbf3a6e9d8ff1ea52
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-02-08 18:54:27 -0300

    add import for controller

--- src/User/index.ts
+++ src/User/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { UserController } from "./UserController";
+import { UserRepository } from "./UserRepository";
+import { UserService } from "./UserService";
 
 export const userRoutes = Router()
 
-const userController = new UserController()
+const userRepository = new UserRepository()
+const userService = new UserService(userRepository)
+const userController = new UserController(userService)
 
 userRoutes.post('/register',(req,res)=>userController.register(req,res))
 userRoutes.post('/login',(req,res)=>userController.login(req,res))
